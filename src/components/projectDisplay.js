697078ec78e1e34be03adb3b8f9b52e5f59c1e17
Megan <13woodruffm@gmail.com>
2020-02-09 22:01:36 -0600

    Add project images and image styling

--- src/components/projectDisplay.js
+++ src/components/projectDisplay.js
@@ -1,61 +1,28 @@
 import React from 'react'
 import { Link, useStaticQuery, graphql } from "gatsby"
 import PropTypes from "prop-types"
-import Img from 'gatsby-image'
+import Img from 'gatsby-image/withIEPolyfill'
 import './projectDisplay.scss'
 
-// import desktop from '../images/stock_image_desk.jpg'
-// import mobile from '../images/stock_image_mobile.jpg'
-const desktop = "stock_image_desk.jpg"
-const mobile = "stock_image_mobile.jpg"
+const ProjectDisplay = () => {
 
-const ProjectDisplay = () => (
-    <div className="projectDisplay">
-        <div className="projectContentContainer">
-            <ProjectCard 
-                className="project" 
-                title="Solve by Every Learner" 
-                description="Helping postsecondary educators learn collectively and connect with peers" 
-                location="Intentional Futures"
-                // skills={["User research", "software architecture design", "product management"]}
-                slug="solve" 
-                imageSource={mobile}
-                />
-            <ProjectCard
-                className="project"
-                title="Learning Session: Equity-Centered Technology Design"
-                location="Bill & Melinda Gates Foundation"
-                description="Helping education leaders at the Bill & Melinda Gates Foundation learn about the equitable design of edtech products"
-                slug="tech_learning_session"
-                imageSource={desktop}
-            />
-            <ProjectCard 
-                className="project" 
-                title="OddJobs" 
-                // skills={["User journey mapping", "IA development", "prototyping"]}
-                location="University of Washington"
-                description="Connecting users with independent service providers in their community" 
-                slug="odd_jobs" 
-                imageSource={mobile}
-            />
-            <ProjectCard 
-                className="project" 
-                title="Rx Connect" 
-                description="Helping primary care providers understand a patient's potential prescription drug costs" 
-                location="University of Washington"
-                // skills={["User research", "problem definition", "prototyping"]}
-                slug="rx_connect" 
-                imageSource={desktop}
-                />
-        </div> 
-    </div>
-)
-
-const ProjectCard = ({ title, description, location, slug, imageSource }) => {
-
- const data = useStaticQuery(graphql`
+    const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "stock_image_desk.jpg" }) {
+      techLearningImage: file(relativePath: { eq: "techlearning.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 700) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      solveImage: file(relativePath: { eq: "everylearnermock.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 700) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      oddJobsImage: file(relativePath: { eq: "oddjobsmockup.png" }) {
         childImageSharp {
           fluid(maxWidth: 700) {
             ...GatsbyImageSharpFluid
@@ -65,11 +32,55 @@ const ProjectCard = ({ title, description, location, slug, imageSource }) => {
     }
   `)
 
+    return (
+        <div className="projectDisplay">
+            <div className="projectContentContainer">
+                <ProjectCard 
+                    className="project" 
+                    title="Solve by Every Learner" 
+                    description="Helping postsecondary educators learn collectively and connect with peers" 
+                    location="Intentional Futures"
+                    // skills={["User research", "software architecture design", "product management"]}
+                    slug="solve" 
+                    imageSource={data.solveImage.childImageSharp.fluid}
+                    />
+                <ProjectCard
+                    className="project"
+                    title="Learning Session: Equity-Centered Technology Design"
+                    location="Bill & Melinda Gates Foundation"
+                    description="Helping education leaders at the Bill & Melinda Gates Foundation learn about the equitable design of edtech products"
+                    slug="tech_learning_session"
+                    imageSource={data.techLearningImage.childImageSharp.fluid}
+                />
+                <ProjectCard 
+                    className="project" 
+                    title="OddJobs" 
+                    // skills={["User journey mapping", "IA development", "prototyping"]}
+                    location="University of Washington"
+                    description="Connecting users with independent service providers in their community" 
+                    slug="odd_jobs" 
+                    imageSource={data.oddJobsImage.childImageSharp.fluid}
+                />
+                {/* <ProjectCard 
+                    className="project" 
+                    title="Rx Connect" 
+                    description="Helping primary care providers understand a patient's potential prescription drug costs" 
+                    location="University of Washington"
+                    // skills={["User research", "problem definition", "prototyping"]}
+                    slug="rx_connect" 
+                    imageSource={data.placeholderImage.childImageSharp.fluid}
+                    /> */}
+            </div> 
+        </div>
+)
+}
+
+const ProjectCard = ({ title, description, location, slug, imageSource }) => {
+
   return (
     <Link className="projectLink" to={`/${slug}`}>
         <div className="projectCard">
-            {/* <img className="projectImage" src={imageSource} alt="Digital application in use"/>  */}
-            <Img className="projectImage" objectFit="cover" fluid={data.placeholderImage.childImageSharp.fluid} alt="Digital application in use" />
+            <Img className="projectImage" objectFit="cover" objectPosition="50% 0" fluid={imageSource} alt="Digital application in use" />
             <div className="projectInfo">
                 <div>
                     <h3 className="projectTitle">{title}</h3>
--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,7 @@ import SEO from "../components/seo"
 const About = () => (
   <Layout>
     <SEO title="About" />
+    {/* <div></div> */}
     <div className="contentWrapper">
       <h1>Thank you for visiting my portfolio!</h1>
       <p style={{padding: '2rem 0 2.4rem'}}>I am a Seattle-based creative technologist who is passionate about the power of cross-discipline collaboration in UX. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet, neque id tempus elementum, justo turpis viverra sem, in semper lorem urna id orci. Suspendisse at efficitur augue. Quisque dignissim ornare consectetur. Ut rutrum vulputate arcu, ut dapibus est blandit ut. Ut eros neque, eleifend non lacinia ut, blandit sit amet nibh. Praesent varius dictum nunc, id sollicitudin metus vestibulum ac. Suspendisse euismod nunc at feugiat varius. Phasellus at suscipit metus. Vestibulum ut velit tincidunt sem laoreet scelerisque sit amet eget dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
