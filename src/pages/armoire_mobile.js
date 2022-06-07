1ad5afedd5c05e8a9ff4e06bfd1ce0060e1f7f0e
Megan <13woodruffm@gmail.com>
2022-06-07 09:54:09 -0500

    Add technical architecture table, content, diagram

--- src/pages/armoire_mobile.js
+++ src/pages/armoire_mobile.js
@@ -64,6 +64,13 @@ const ArmoireMobile = () => {
           }
         }
       }
+      technicalArchitecture: file(relativePath: { eq: "Mobile-TechnicalArchitectureTransparent.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
       case: file(relativePath: { eq: "Mobile-MyCase.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
@@ -126,82 +133,215 @@ const ArmoireMobile = () => {
            In response to <a href="#customerDemand">strong user demand</a>, I led the end-to-end creation of Armoire's mobile app. In addition to developing an ambitious <a href="#mvpRequirements">MVP feature set</a>, I designed the app's <a href="#technicalArchitecture">technical architecture</a>, devised its <a href="#informationArchitecture">IA and navigation</a>, and built its <a href="#componentLibrary">UI component library</a>. Along the way, I learned to optimize JavaScript performance and React Native animations, and I pushed to improve existing product experiences whenever possible.
           </p>
           <p>
-            Upon successful launch, the apps were quickly adopted by more than 60% of Armoire's user base and received overwhelmingly positive reviews. Their improved UX and native capabilities increased user engagement and retention. 
+            Upon successful launch, the apps quickly became the primary platform for over 70% of Armoire's engaged users. Their improved UX and native capabilities also increased user retention. 
         </p>
       </div>
       <div id="customerDemand" className="bodyTextWrapper intro">
         <h2>Context and Requirements</h2>
         <h3 className="subhead">Why build an app?</h3>
+        <h4>Customer demand</h4>
         <p>
-          When I conducted my first set of user interviews with Armoire members in May 2020, <em>every single interviewee</em> identified a mobile app as their number one ask for our tech team.
+          When I conducted my first set of user interviews with Armoire members in May 2020, <em>every single interviewee</em> identified a mobile app as their number one ask for our tech team. This user mandate for a mobile app was clear and unrelenting across multiple customer surveys and interviews. Though Armoire's web app contained all of the core functionality needed to rent clothing, users wanted the workflow to be seemlessly integrated on their mobile devices.
         </p>
+        <h4>Business imperative</h4>
         <p>
-          This was not a coincidence; the user mandate for a mobile app was clear and unrelenting across multiple customer surveys and interviews. Though Armoire's web experience ...</p>
-        <p>The business case was also compelling:
-          <ul className="mt-3">
-            <li>Key competitors in our market (Rent the Runway, Nuuly, Stitchfix) all had mobile apps available on iOS</li>
-            <li>Mobile apps would unlock native-only capabilities that could increase user convenience and engagement</li>
-          </ul>
+          The business case was also compelling. Key competitors in our market (Rent the Runway, Nuuly, Stitchfix) all had mobile apps. We also knew that delivering a mobile app would unlock native-only capabilities that could increase user convenience, engagement, and ultimately, satisfaction.
         </p>  
       </div>
       <div id="mvpRequirements" className="bodyTextWrapper">
         <h3>MVP Requirements</h3>
         <p>To make the transition to mobile smooth for our members, I knew the MVP would require building almost all of Armoire's existing functionality into the mobile app. The app would be able to use the same server APIs and protocol as our web app, so the technical effort was almost entirely on the client side.</p> 
-        <table>
+      </div>
+      <table>
           <thead>
             <tr>
               <th></th>
-              <th>Features</th>
+              <th>Feature Description</th>
               <th>Existing Experience</th>
             </tr>
           </thead>
           <tbody>
           <tr>
             <th>Closet</th>
-            <td></td>
-            <td></td>
+            <td>
+              <ul>
+                <li>Core experience for finding and saving styles</li>
+                <li>User's recommendations and favorites sections</li>
+                <li>Product list views (sections)</li>
+                <li>Product details page</li>
+              </ul>
+            </td>
+            <td>
+
+            </td>
           </tr>
           <tr>
             <th>Search</th>
-            <td></td>
+            <td>
+              <ul>
+                <li>
+                  Search  across all of Armoire's inventory and user-generated content.
+                </li>
+              </ul>
+            </td>
             <td></td>
           </tr>
           <tr>
             <th>Case</th>
-            <td></td>
+            <td>
+              <ul>
+                <li>Selection and confirmation of styles a user is going to rent</li>
+                <li>Package tracking</li>
+                <li>Reviewing + returning items</li>
+                <li>Purchasing items</li>
+              </ul>
+            </td>
             <td></td>
           </tr>
           <tr>
             <th>PWR Feed</th>
-            <td></td>
+            <td>
+              <ul>
+                  <li>Viewing and sharing looks to the members-only style feed</li>
+                  <li>Commenting, liking, and favoriting items on looks</li>
+                  <li>Viewing other users' profiles</li>
+                  <li>Updating personal profile</li>
+                  <li>Following other members</li>
+                </ul>
+            </td>
             <td></td>
           </tr>
           <tr>
             <th>Notifications</th>
-            <td></td>
+            <td>
+              <ul>
+                  <li>Notifications inbox</li>
+                  <li>Notifications settings</li>
+                  <li>Push Notification integration</li>
+                </ul>
+            </td>
             <td></td>
           </tr>
           <tr>
             <th>Account</th>
-            <td></td>
+            <td>
+              <ul>
+                <li>Subscription updates</li>
+                <li>Shipping and billing updates</li>
+                <li>Return tracking</li>
+                <li>Account and password management</li>
+                <li>Rewards</li>
+              </ul>
+            </td>
             <td></td>
           </tr>
           </tbody>
         </table>
-      </div>
       <div id="technicalArchitecture" className="bodyTextWrapper">
         <h2>Technical Design</h2>
         <h3>Investigation and Proof of Concept</h3>
         <p>
-          As a web developer with React experience, I held a strong bias towards building Android and iOS apps using a common React Native codebase. However, with 85% of Armoire's user base on iOS, I also did not want to rule out the potential advantages of writing a native app on iOS only.
+          My first goal was to determine an overarching framework and platform for the app's development.
         </p>
         <p>
-          I spent the first two weeks of the project investigating the technical limitations of React Native by building a proof-of-concept prototype. I used the Expo Managed Framework to minimize startup time and to answer questions about the advantages and limitations of the Expo Managed framework vs a bare React Native app. The table below lists the technical considerations I weighed after two weeks of research and prototyping.
-          </p>
+          As a web developer with React experience, I held a strong bias towards building Android and iOS apps using a common <a href="https://reactnative.dev/" target="_blank">React Native</a> codebase. However, with 85% of Armoire's user base on iOS, I also did not want to rule out the potential advantages of writing a native app on iOS only.
+        </p>
+        <p>
+          I spent the first two weeks of the project investigating the technical limitations of React Native by building a proof-of-concept prototype. I used the <a href="https://docs.expo.dev/workflow/already-used-react-native/#expo-managed-workflow" target="_blank">Expo Managed Framework</a> to minimize development startup time and to also answer questions about the advantages and limitations of using it longterm vs a bare React Native app. The table below lists the technical considerations I weighed after two weeks of research and prototyping.
+        </p>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>iOS Native (Swift)</th>
+              <th>React Native </th>
+              <th>React Native + Expo Managed</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <th>App startup speed and performance</th>
+            <td>✅  Fastest</td>
+            <td>⚠️  Slower than native ios app</td>
+            <td>⚠️  Slower than native ios app</td>
+          </tr>
+          <tr>
+            <th>App bundle size</th>
+            <td>✅  Smallest</td>
+            <td>⚠️  Larger</td>
+            <td>⛔  Largest. All Expo packages are required in JS bundle.</td>
+          </tr>
+          <tr>
+            <th>Native Code</th>
+            <td>⛔  Required</td>
+            <td>⚠️  Minimal. Primarily needed for configuring third-party packages, Push Notifications, and Deeplinks. Estimated 10% of work.</td>
+            <td>✅  Not required. Completely abstracted away by Expo.</td>
+          </tr>
+          <tr>
+            <th>Developer Learning Curve</th>
+            <td>⛔  Highest</td>
+            <td>⚠️  Minimal</td>
+            <td>✅  Lowest</td>
+          </tr>
+          <tr>
+            <th>Push Notification Support</th>
+            <td>✅  Rich</td>
+            <td>✅  Rich</td>
+            <td>⛔  Minimal. Only supported through Expo Notifications Service.</td>
+          </tr>
+          <tr>
+            <th>Process for builds and submissions</th>
+            <td>⛔  Difficult. Would need to build manually each time, or invest in third-party build and submit provider.</td>
+            <td>✅  Easy. Can use Expo Application Services with some initial configuration.</td>
+            <td>✅  Easiest. Can use Expo Application Services with minimal configuration.</td>
+          </tr>
+          <tr>
+            <th>Process for publishing small updates</th>
+            <td>⛔  Time-consuming. App store submission and review required for every update.</td>
+            <td>✅  Small updates that don’t impact JS packages can be published “over the air” with Expo, without app store review.</td>
+            <td>✅  Small updates that don’t impact JS packages can be published “over the air” with Expo, without app store review.</td>
+          </tr>
+          <tr>
+            <th>Android app</th>
+            <td>⛔  Out of scope</td>
+            <td>⚠️  Supported with minimal Android-only configuration and testing</td>
+            <td>✅  Automatically supported with no extra work.</td>
+          </tr>
+          <tr>
+            <th>Support for critical third-party packages (Stripe, Intercom)</th>
+            <td>✅  Complete support</td>
+            <td>✅  Near-complete support. Packages in development at time of investigation.</td>
+            <td>⛔  Minimal. Any package that requires native configuration cannot be used.</td>
+          </tr>
+          </tbody>
+        </table>
+      <div className="bodyTextWrapper">
+        <p>
+          The initial React Native prototype I created had a basic login screen, tab navigation, a closet screen with horizontal sliding sections, and the bones of a product details page. I also tested out image upload, state management with mobx, font customizations, and using Sass as a styling preprocessor.
+        </p>
+        <p>
+          Overall, I found the development process for this POC to be relatively straightforward given my previous knowledge of React, though there were certainly some "gotchas" I had to adapt to.
+        </p>
         <h3>Technical Architecture</h3>
+        <p>Using my analysis and proof of concept I developed a high-level technical architecture for the app.</p>
+        <p>
+          Since I knew Armoire's target users largely used high-end devices on strong data connections, I decided that the speed and size advantages of a native-built iOS app were not worth its high development learning curve and slow update cadence. Additionally, building in React Native would mean that Armoire would be alone amongst its competitors in offering users an Android app.
+        </p>
+        <p>
+          Though the Expo Managed workflow initially seemed like a winner based on its extremely low start-up cost and complete abstraction of all native code, I identified enough limitations in Expo's Push Notification infrastructure and support for critical third-party libraries (e.g. Stripe, Intercom) to decide that a bare React Native app was the best framework for this project.
+        </p>
+        <p>
+          From this base, I used React Native documentation, forums, and community-vetted packages to outline a basic architecture (below) for the various required pieces of functionality in the MVP. 
+        </p>
+        
       </div>
+      <Img
+        style={{ maxWidth: 800, margin: '0 auto'}}
+        alt=""
+        fluid={data.technicalArchitecture.childImageSharp.fluid}/>
       <div id="informationArchitecture" className="bodyTextWrapper">
-        <h2>Foundational UX Design</h2>
+        <h2>Foundational UX Development</h2>
         <h3>Information Architecture and Navigation</h3>
         <h3>Component Library</h3>
       </div>
@@ -222,13 +362,13 @@ const ArmoireMobile = () => {
 
       <div className="bodyTextWrapper overview">
         <h3 className="sectionHeading">Download the App</h3>
-        <p className="">The released mobile apps are available for download on the <a
+        <p className="">The mobile apps were released in early April 2021 and are available for download on the <a
             href="https://apps.apple.com/us/app/armoire-style/id1560230748"
             target="_blank"
             rel="noopener noreferrer"
           >Apple</a> and <a href="https://play.google.com/store/apps/details?id=style.armoire.armoiremobile&hl=en_US&gl=US" target="_blank"
             rel="noopener noreferrer"
-          >Google Play</a> stores. I will be publishing more soon on the development process, technical challenges, UX improvements, and outcomes. In the meantime, feel free to download and explore the app or check out the overview below.</p>
+          >Google Play</a> stores.</p>
       </div>
       <div className="bodyTextWrapper">
         <h4 className="appSection first">My Closet</h4>
